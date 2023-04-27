f17d22fd8a188f79b2e155074bfbf064edd0d2e4
Anshujdhakad77 <anshujdhakad77@gmail.com>
2023-04-28 00:52:46 +0530

    complete task

--- b22 1/script.js
+++ b22 1/script.js
@@ -9,11 +9,8 @@ function init() {
     });
     // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
     locoScroll.on("scroll", ScrollTrigger.update);
-    locoScroll.on("resize",function(){
-        location.reload()
-        locoScroll.update()
-    })
-    
+
+
     // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
     ScrollTrigger.scrollerProxy("#main", {
         scrollTop(value) {
@@ -46,28 +43,6 @@ function init() {
 
 init()
 
-
-document.querySelector("#page3").addEventListener("mousemove", function (dets) {
-    document.querySelector("#page3 #img-div").style.left = `${dets.x}px`
-    document.querySelector("#page3 #img-div").style.top = `${dets.y}px`
-})
-
-document.querySelector("#page4").addEventListener("mousemove", function (dets) {
-    document.querySelector("#page4>img").style.left = (dets.x-30) + "px"
-    document.querySelector("#page4>img").style.top = (dets.y-10) + "px"
-    document.querySelector("#page4>button").style.left = (dets.x + 10) + "px"
-    document.querySelector("#page4>button").style.top = (dets.y + 200) + "px"
-})
-
-var elem = document.querySelectorAll(".elem")
-elem.forEach(function(e){
-    var a = e.getAttribute("data-img")
-    e.addEventListener("mouseenter",function(){
-        document.querySelector("#page4>img").setAttribute("src",a)
-    })
-})
-
-
 var overlay = document.querySelector("#overlay")
 var iscroll = document.querySelector("#scroll")
 
@@ -82,15 +57,58 @@ overlay.addEventListener("mousemove", function (dets) {
     iscroll.style.top = `${dets.y - 38}px`
 })
 
+document.querySelector("#page3").addEventListener("mousemove", function (dets) {
+    document.querySelector("#page3 #img-div").style.left = `${dets.x}px`
+    document.querySelector("#page3 #img-div").style.top = `${dets.y}px`
+})
 
-gsap.to("#page2 img",{
-    rotate:-5,
-    scrollTrigger:{
-        scroller:"#main",
-        trigger:"#page2 img",
-        start:"top 80%",
-        markers:true,
-        scrub:3
+document.querySelector("#page4").addEventListener("mousemove", function (dets) {
+    document.querySelector("#page4>img").style.left = dets.x + "px"
+    document.querySelector("#page4>img").style.top = dets.y + "px"
+    document.querySelector("#page4>button").style.left = (dets.x + 50) + "px"
+    document.querySelector("#page4>button").style.top = (dets.y + 200) + "px"
+})
+
+var elem = document.querySelectorAll(".elem")
+elem.forEach(function (e) {
+    var a = e.getAttribute("data-img")
+    e.addEventListener("mouseenter", function () {
+        document.querySelector("#page4>img").setAttribute("src", a)
+    })
+})
+$('#page1 h1').textillate({
+    in: {
+        effect: 'fadeInUp',
+        delayScale: 1,
+    }
+});
+
+gsap.from("#page2 h1", {
+    duration: 0.5,
+    onStart: function () {
+        $('#page2 h1').textillate({
+            in: {
+                effect: 'bounceIn',
+                delayScale: 0.5,
+            }
+        });
+    },
+    scrollTrigger: {
+        trigger: "#page2 h1",
+        scroller: "#main",
+        start: "top 90%"
+    }
+})
+
+
+gsap.to("#page2 img", {
+    rotate: -5,
+    scrollTrigger: {
+        scroller: "#main",
+        trigger: "#page2 img",
+        start: "top 80%",
+        // markers: true,
+        scrub: 3
     }
 })
 
@@ -108,42 +126,61 @@ gsap.to("#main", {
 })
 
 
-
-
-
-gsap.from("#page1 h1",{
-    duration:0.5,
-    onStart:function(){
-        $('#page1 h1').textillate({ in: { effect: 'fadeInUp' } });
+// Make a timeline to pin svg and change color of nav to black
+var tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: "svg",
+        scroller: "#main",
+        // markers: true,
+        start: "top 0%",
+        end: "top -200%",
+        scrub: true,
     }
 })
 
+tl.to("svg", {
+    scale: 1,
+    top: "5%",
+    fill: "#111",
 
-gsap.from("#page2 h1", {
-    duration: 0.5,
-    onStart: function () {
-        $('#page2 h1').textillate({ in: { effect: 'fadeInUp',   delayScale: 0.5, } });
-    },
-    scrollTrigger:{
-        trigger:"#page2 h1",
-        scroller:"#main",
-        start:"top 80%",
-        scrub:2
-    }
 })
 
+tl.to("#nav", {
+    color: "#111",
+    background: "linear-gradient(#ffffffeb,#ffffff6e,#ffffff00)",
+})
 
+// Make a timeline again to change color of nav and svg to white
 
+var tl2 = gsap.timeline({
+    scrollTrigger: {
+        trigger: "svg",
+        scroller: "#main",
+        // markers: "true",
+        start: "top -340%",
+        end: "top -340%",
+        scrub: true,
+    }
+})
+tl2.to("svg", {
+    scale: 1,
+    top: "5%",
+    fill: "#fff",
 
+})
+tl2.to("#nav", {
+    color: "#fff",
+    background: "linear-gradient(#000000d5,#00000089,#00000000)",
+})
 
 // Make a timeline to pin svg and change color of nav to black
 var tl = gsap.timeline({
     scrollTrigger: {
         trigger: "svg",
         scroller: "#main",
-        // markers: "true",
-        start: "top 45%",
-        end: "top -50%",
+        // markers: true,
+        start: "top 0%",
+        end: "top -140%",
         scrub: true,
     }
 })
@@ -184,43 +221,47 @@ tl2.to("#nav", {
     background: "linear-gradient(#000000d5,#00000089,#00000000)",
 })
 
-gsap.to("#page5",{
-    scrollTrigger:{
-        trigger:"#page5",
-        scroller:"#main",
-        start:"top 0%",
-        end:"top -100%",
-        scrub:true,
-        pin:true,
-        markers:true
+
+
+// page5 animation to pin the elements 
+
+gsap.to("#page5", {
+    scrollTrigger: {
+        trigger: "#page5",
+        scroller: "#main",
+        start: "top 0%",
+        end: "top -100%",
+        scrub: true,
+        pin: true,
+        // markers: true
+    }
+})
+gsap.from("#page5-div1", {
+    rotate: -5,
+    scrollTrigger: {
+        trigger: "#page5-div1",
+        scroller: "#main",
+        start: "top 85%%",
+        end: "top 30%",
+        // markers: true,
+        scrub: true,
     }
 })
 
-
-gsap.from("#page5-div1",{
-    y:50,
-    scrollTrigger:{
-        trigger:"#page5-div1",
-        scroller:"#main",
-        start:"top 73%",
-        end:"top 70%",
-        markers:true,
-        scrub:1
+gsap.from("#page5-div2", {
+    y: 570,
+    rotate: -15,
+    scrollTrigger: {
+        trigger: "#page5-div2",
+        scroller: "#main",
+        start: "top 80%",
+        end: "top 50%",
+        scrub: 2,
+        // markers: true
     }
 })
 
-
-gsap.from("#page5-div2",{
-    y:600,
-    scrollTrigger:{
-        trigger:"#page5-div2",
-        scroller:"#main",
-        start:"top 80%",
-        end:"top 50%",
-        scrub:2,
-        markers:true
-    }
-})
+// Code to see full screen nav 
 
 document.querySelector("#nav i").addEventListener("click",function(){
     document.querySelector("#full-scr").style.top = "0vh"
@@ -228,33 +269,4 @@ document.querySelector("#nav i").addEventListener("click",function(){
 document.querySelector("#full-scr i").addEventListener("click",function(){
     document.querySelector("#full-scr").style.top = "-100vh"
 })
-// gsap.from("#page5-div2",{
-//     y:400,
-//     scrollTrigger:{
-//         trigger:"#page5-div2",
-//         scroller:"#main",
-//         start:"top 85%",
-//         end:"top 82%",
-//         markers:true,
-//         scrub:1
-//     }
-// })
 
-
-
-
-
-// you can write that code to automatically refresh the page whenever you resize window 
-
-// window.addEventListener("resize",function(){
-//     location.reload()
-// })
-
-
-// window.addEventListener("resize", function () {
-//     var abc = document.querySelector("#main")
-//     console.log()
-//     if(abc.getBoundingClientRect().width>600){
-//         console.log("hyeloenheye")
-//     }
-// })
